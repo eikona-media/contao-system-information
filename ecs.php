0ec039b6889b6741809ca5c3cc37feaeccdcde4e
Ralf Baussenwein <ralf@bwein.net>
2022-12-03 16:49:11 +0100

    Update ecs config

--- ecs.php
+++ ecs.php
@@ -2,44 +2,33 @@
 
 declare(strict_types=1);
 
+use Contao\EasyCodingStandard\Fixer\TypeHintOrderFixer;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
 use SlevomatCodingStandard\Sniffs\TypeHints\DisallowArrayTypeHintSyntaxSniff;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $vendorDir = __DIR__ . '/vendor';
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([__DIR__.'/tools/ecs/vendor/contao/easy-coding-standard/config/contao.php']);
 
-    if (!is_dir($vendorDir)) {
-        $vendorDir = __DIR__ . '/../../vendor';
-    }
+    $ecsConfig->skip([
+        MethodChainingIndentationFixer::class   => [
+            '*/DependencyInjection/Configuration.php',
+            '*/Resources/config/*.php',
+        ],
+        TypeHintOrderFixer::class,
+        DisallowArrayTypeHintSyntaxSniff::class => ['*Model.php'],
+        '*/templates/*.html5',
+    ]);
 
-    $containerConfigurator->import($vendorDir . '/contao/easy-coding-standard/config/self.php');
+    $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
+        'header' => "This file is part of System Information Bundle for Contao Open Source CMS.\n\n(c) eikona-media.de\n\n@license MIT",
+    ]);
 
-    $parameters = $containerConfigurator->parameters();
+    $ecsConfig->parallel();
+    $ecsConfig->lineEnding("\n");
 
-    $skips                                          = [];
-    $skips[MethodChainingIndentationFixer::class]   = ['*/DependencyInjection/Configuration.php'];
-    $skips[DisallowArrayTypeHintSyntaxSniff::class] = ['*Model.php'];
-
-    if (defined(Option::class . '::EXCLUDE_PATHS')) {
-        $parameters->set(Option::EXCLUDE_PATHS, ['*/templates/*.html5']);
-    } else {
-        $skips[] = '*/templates/*.html5';
-    }
-
-    $parameters->set(Option::SKIP, $skips);
-
-    $services = $containerConfigurator->services();
-    $services
-        ->set(HeaderCommentFixer::class)
-        ->call(
-            'configure',
-            [
-                [
-                    'header' => "This file is part of System Information Bundle for Contao Open Source CMS.\n\n(c) eikona-media.de\n\n@license MIT",
-                ]
-            ]
-        );
+    $parameters = $ecsConfig->parameters();
+    $parameters->set(Option::CACHE_DIRECTORY, sys_get_temp_dir().'/ecs_default_cache');
 };
